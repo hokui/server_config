1660f50ecf570c36769951a038084a00776c2477
iTakeshi <takeshi.ito.doraemon@gmail.com>
2015-04-04 11:22:46 +0900

    kill old containers before run new ones

--- run.sh
+++ run.sh
@@ -14,6 +14,8 @@ cd ~/src/github.com/hokui/mailing_list
 git pull origin master
 docker build -t hokui/mailing_list .
 
+docker kill $(docker ps -q)
+
 docker run -d -v /var/data/hokui:/var/data/hokui --name hokui_data_production hokui/data
 docker run -d --net=host -v /var/log/hokui/nginx_proxy/nginx:/var/log/nginx nginx_proxy
 docker run -d --net=host -v /var/log/hokui/app/supervisor:/var/log/supervisor -v /var/log/hokui/app/nginx:/var/log/nginx -v /var/log/hokui/app/rails:/var/app/log --volumes-from hokui_data_production hokui/app
