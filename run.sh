8ec756a98779f80abc5b39de7f02c749f951160a
iTakeshi <takeshi.ito.doraemon@gmail.com>
2015-04-04 15:34:51 +0900

    fix volume container

--- run.sh
+++ run.sh
@@ -2,7 +2,6 @@
 
 cd ~/src/github.com/hokui/server_config
 git pull origin master
-docker build -t hokui/data ./data
 docker build -t hokui/nginx_proxy ./nginx_proxy
 docker build -t hokui/ruby_nginx_base ./ruby_nginx_base
 
@@ -18,7 +17,14 @@ docker build -t hokui/mailing_list .
 
 docker kill $(docker ps -q)
 
-docker run -d -v /var/data/hokui:/var/data/hokui --name hokui_data_production hokui/data
+docker ps -a | grep hokui_data_production
+if [ "$?" -eq 0 ]
+then
+  # do nothing
+else
+  docker create -v /var/data/hokui --name hokui_data_production busybox
+fi
+
 docker run -d --net=host -v /var/log/hokui/nginx_proxy/nginx:/var/log/nginx hokui/nginx_proxy
 docker run -d --net=host -v /var/log/hokui/app/supervisor:/var/log/supervisor -v /var/log/hokui/app/nginx:/var/log/nginx -v /var/log/hokui/app/rails:/var/app/log --volumes-from hokui_data_production hokui/app
 docker run -d --net=host -v /var/log/hokui/mailing_list/rails:/var/app/log --volumes-from hokui_data_production hokui/mailing_list
